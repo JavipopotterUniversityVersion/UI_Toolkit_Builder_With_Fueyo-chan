8e3d2f76b95e2413910312d3ca1157cfea6b9bb7
Javipopotter <javivals@ucm.es>
2025-02-13 15:10:21 +0100

    uwu

---
+++ UI_Toolkit_Builder/Assets/Scripts/Lab2.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public struct PestCont
+{
+    public VisualElement pest;
+    public VisualElement cont;
+}
+
+public class Lab2 : MonoBehaviour
+{
+    PestCont[] lista_pc = new PestCont[3];
+
+    private void OnEnable() {
+        UIDocument uiDoc = GetComponent<UIDocument>();
+        VisualElement root = uiDoc.rootVisualElement;
+
+        VisualElement pestanyas = root.Q<VisualElement>("Pestanyas");
+        VisualElement contenido = root.Q<VisualElement>("Contenido");
+
+        lista_pc[0].pest = pestanyas.Q<VisualElement>("Azul");
+        lista_pc[0].cont = contenido.Q<VisualElement>("Azul");
+
+        lista_pc[1].pest = pestanyas.Q<VisualElement>("Rojo");
+        lista_pc[1].cont = contenido.Q<VisualElement>("Rojo");
+
+        lista_pc[2].pest = pestanyas.Q<VisualElement>("Verde");
+        lista_pc[2].cont = contenido.Q<VisualElement>("Verde");
+
+        foreach (PestCont pc in lista_pc)
+        {
+            pc.pest.RegisterCallback<MouseDownEvent>(e => {
+                NoContenido();
+                pc.cont.style.display = DisplayStyle.Flex;
+            });
+        }
+    }
+
+    void NoContenido()
+    {
+        foreach (PestCont pc in lista_pc)
+        {
+            pc.cont.style.display = DisplayStyle.None;
+        }
+    }
+}
