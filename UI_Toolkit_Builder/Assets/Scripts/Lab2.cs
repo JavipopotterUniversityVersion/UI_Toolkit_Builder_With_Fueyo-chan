6ef5176d7ba1aab40ea12ea1b4acc21289e5e5ef
JaviFueyo <jfueyo@ucm.es>
2025-02-20 16:13:09 +0100

    Prac 2 acabada

--- UI_Toolkit_Builder/Assets/Scripts/Lab2.cs
+++ UI_Toolkit_Builder/Assets/Scripts/Lab2.cs
@@ -18,29 +18,47 @@ public class Lab2 : MonoBehaviour
         UIDocument uiDoc = GetComponent<UIDocument>();
         VisualElement root = uiDoc.rootVisualElement;
 
-        VisualElement pestanyas = root.Q<VisualElement>("Pestanyas");
         VisualElement contenido = root.Q<VisualElement>("Contenido");
+        VisualElement pestanyas = contenido.Q<VisualElement>("Pestanyas");
+        List<VisualElement> returnButtons = new List<VisualElement>();
 
         lista_pc[0].pest = pestanyas.Q<VisualElement>("Azul");
+        lista_pc[0].cont = contenido.Q<VisualElement>("Azul");
+        returnButtons.Add(lista_pc[0].cont.Q<VisualElement>("return"));
+
         Label texto = lista_pc[0].pest.Q<Label>("Texto");
         texto.text = @"<rotate=""2"">Inventario</rotate>";
-        lista_pc[0].cont = contenido.Q<VisualElement>("Azul");
+
 
         lista_pc[1].pest = pestanyas.Q<VisualElement>("Rojo");
+        lista_pc[1].cont = contenido.Q<VisualElement>("Rojo");
+        returnButtons.Add(lista_pc[1].cont.Q<VisualElement>("return"));
+
         texto = lista_pc[1].pest.Q<Label>("Texto");
         texto.text = @"<b><gradient=""Fueyo"">Mejoras</gradient></b>";
-        lista_pc[1].cont = contenido.Q<VisualElement>("Rojo");
+
 
         lista_pc[2].pest = pestanyas.Q<VisualElement>("Verde");
+        lista_pc[2].cont = contenido.Q<VisualElement>("Verde");
+        returnButtons.Add(lista_pc[2].cont.Q<VisualElement>("return"));
+
         texto = lista_pc[2].pest.Q<Label>("Texto");
         texto.text = @"Estadísticas";
-        lista_pc[2].cont = contenido.Q<VisualElement>("Verde");
 
         foreach (PestCont pc in lista_pc)
         {
             pc.pest.RegisterCallback<MouseDownEvent>(e => {
+                pestanyas.style.display = DisplayStyle.None;
                 NoContenido();
                 pc.cont.style.display = DisplayStyle.Flex;
+            });
+        }
+
+        foreach (VisualElement ret in returnButtons)
+        {
+            ret.RegisterCallback<MouseDownEvent>(e => {
+                NoContenido();
+                pestanyas.style.display = DisplayStyle.Flex;
             });
         }
 
@@ -50,9 +68,9 @@ public class Lab2 : MonoBehaviour
         VisualElement mtracker = root.Q<VisualElement>("unity-tracker");
         mtracker.style.backgroundColor = Color.yellow;
 
-        Label sliderText = mslider.Q<Label>("Label");
-        sliderText.text = @"<color=""FF00FF"">Dificultad</color>";
-        sliderText.style.fontSize = 16;
+        //Label sliderText = mslider.Q<Label>("Label");
+        //sliderText.text = @"<color=""FF00FF"">Dificultad</color>";
+        //sliderText.style.fontSize = 16;
     }
 
     void NoContenido()
