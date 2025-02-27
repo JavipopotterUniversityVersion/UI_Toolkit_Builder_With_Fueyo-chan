a3f4712c0e08568df9e74c45a2ee8e18de6762f7
JaviFueyo <jfueyo@ucm.es>
2025-02-27 16:50:12 +0100

    Aiuda

--- UI_Toolkit_Builder/Assets/Scripts/Lab3Dragger.cs
+++ UI_Toolkit_Builder/Assets/Scripts/Lab3Dragger.cs
@@ -14,7 +14,7 @@ public class Lab3Dragger : PointerManipulator
     public Lab3Dragger()
     {
         m_PointerId = -1;
-        activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
+        activators.Add(new ManipulatorActivationFilter { button = UnityEngine.UIElements.MouseButton.LeftMouse });
         m_Active = false;
     }
 
@@ -24,5 +24,58 @@ public class Lab3Dragger : PointerManipulator
         target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
         target.RegisterCallback<PointerUpEvent>(OnPointerUp);
 
+    }
+    protected override void UnregisterCallbacksFromTarget()
+    {
+        target.RegisterCallback<PointerDownEvent>(OnPointerDown);
+        target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        target.RegisterCallback<PointerUpEvent>(OnPointerUp);
+
+    }
+
+    protected void OnPointerDown(PointerDownEvent e)
+    {
+        if (m_Active)
+        {
+            e.StopImmediatePropagation();
+            return;
+        }
+
+        if(CanStartManipulation(e))
+        {
+            m_start = e.localPosition;
+            m_PointerId = e.pointerId;
+
+            m_Active = true;
+            target.CapturePointer(m_PointerId);
+            e.StopPropagation();
+        }
+    }
+
+    protected void OnPointerMove(PointerMoveEvent e)
+    {
+        if (!m_Active || !target.HasPointerCapture(m_PointerId))
+        {
+            return;
+        }
+
+        Vector2 diff = e.localPosition - m_start;
+        Debug.Log(diff);
+        target.style.top = target.layout.y + diff.y;
+        target.style.left = target.layout.x + diff.x;
+
+        e.StopPropagation();
+    }
+
+    protected void OnPointerUp(PointerUpEvent e)
+    {
+        if (!m_Active || !target.HasPointerCapture(m_PointerId) || !CanStopManipulation(e))
+        {
+            return;
+        }
+
+        m_Active = false;
+        target.ReleaseMouse();
+        e.StopPropagation();
     }
 }
--- UI_Toolkit_Builder/Assets/Scripts/lab3.cs
+++ UI_Toolkit_Builder/Assets/Scripts/lab3.cs
@@ -11,22 +11,10 @@ public class lab3 : MonoBehaviour
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
         VisualElement container = root.Q<VisualElement>("Container");
-        container.Query<VisualElement>("Row").ForEach(row =>
+        container.Query<VisualElement>("item1").ForEach(elem =>
         {
-            row.RegisterCallback<ClickEvent>(evt =>
-            {
-                var target = (evt.target as VisualElement);
-                if (target != row)
-                {
-                    //target.style.backgroundColor = new Color(0.5f, 0.5f, 0.5f);
-                    
-                }
-            }, TrickleDown.TrickleDown);
-
-            List<VisualElement> rowElems = row.Children().ToList();
-            rowElems.ForEach(elem => {
-                elem.AddManipulator(new Lab3Manipulator());
-                });
+            elem.AddManipulator(new Lab3Manipulator());
+            elem.AddManipulator(new Lab3Dragger());
         });
     }
 }
