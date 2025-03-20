6fcd32aa8f148d80ba2cb04ada043a2b1b721c4b
JaviFueyo <jfueyo@ucm.es>
2025-03-20 15:25:49 +0100

    Lo q hice del 2

--- UI_Toolkit_Builder/Assets/Scripts/Lab3Manipulator.cs
+++ UI_Toolkit_Builder/Assets/Scripts/Lab3Manipulator.cs
@@ -12,17 +12,33 @@ public class Lab3Manipulator : MouseManipulator
     }
     protected override void RegisterCallbacksOnTarget()
     {
-        target.RegisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
+        //target.RegisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
+        target.RegisterCallback<MouseOverEvent>(MouseOver, TrickleDown.TrickleDown);
     }
 
     protected override void UnregisterCallbacksFromTarget()
     {
-        target.UnregisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
+        //target.UnregisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
+        target.UnregisterCallback<MouseOverEvent>(MouseOver, TrickleDown.TrickleDown);
     }
 
     void MouseDown(MouseDownEvent clk)
     {
         Debug.Log(target.name + ": Click en Elemento");
+        if (CanStartManipulation(clk))
+        {
+            Debug.Log("a");
+            target.style.borderBottomColor = new Color(1, 1, 1, 1);
+            target.style.borderTopColor = new Color(1, 1, 1, 1);
+            target.style.borderLeftColor = new Color(1, 1, 1, 1);
+            target.style.borderRightColor = new Color(1, 1, 1, 1);
+            clk.StopPropagation();
+        }
+    }
+
+    void MouseOver(MouseOverEvent clk)
+    {
+        Debug.Log(target.name + ": Puntero sobre elemento");
         if (CanStartManipulation(clk))
         {
             Debug.Log("a");
