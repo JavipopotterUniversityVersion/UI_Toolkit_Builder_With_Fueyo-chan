559ba976eea58d94a9623f79791d3b387d563412
Javipopotter <javivals@ucm.es>
2025-03-20 15:30:16 +0100

    Update Lab3Manipulator.cs

--- UI_Toolkit_Builder/Assets/Scripts/Lab3Manipulator.cs
+++ UI_Toolkit_Builder/Assets/Scripts/Lab3Manipulator.cs
@@ -8,18 +8,20 @@ public class Lab3Manipulator : MouseManipulator
 {
     public Lab3Manipulator()
     {
-        activators.Add(new ManipulatorActivationFilter { button = MouseButton.RightMouse });
+        // activators.Add(new ManipulatorActivationFilter { button = MouseButton.RightMouse });
     }
     protected override void RegisterCallbacksOnTarget()
     {
         //target.RegisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
         target.RegisterCallback<MouseOverEvent>(MouseOver, TrickleDown.TrickleDown);
+        target.RegisterCallback<MouseOutEvent>(MouseOut, TrickleDown.TrickleDown);
     }
 
     protected override void UnregisterCallbacksFromTarget()
     {
         //target.UnregisterCallback<MouseDownEvent>(MouseDown, TrickleDown.TrickleDown);
         target.UnregisterCallback<MouseOverEvent>(MouseOver, TrickleDown.TrickleDown);
+        target.UnregisterCallback<MouseOutEvent>(MouseOut, TrickleDown.TrickleDown);
     }
 
     void MouseDown(MouseDownEvent clk)
@@ -39,14 +41,22 @@ public class Lab3Manipulator : MouseManipulator
     void MouseOver(MouseOverEvent clk)
     {
         Debug.Log(target.name + ": Puntero sobre elemento");
-        if (CanStartManipulation(clk))
-        {
-            Debug.Log("a");
-            target.style.borderBottomColor = new Color(1, 1, 1, 1);
-            target.style.borderTopColor = new Color(1, 1, 1, 1);
-            target.style.borderLeftColor = new Color(1, 1, 1, 1);
-            target.style.borderRightColor = new Color(1, 1, 1, 1);
-            clk.StopPropagation();
-        }
+        Debug.Log("a");
+        target.style.borderBottomColor = new Color(1, 1, 1, 1);
+        target.style.borderTopColor = new Color(1, 1, 1, 1);
+        target.style.borderLeftColor = new Color(1, 1, 1, 1);
+        target.style.borderRightColor = new Color(1, 1, 1, 1);
+        clk.StopPropagation();
+    }
+
+    void MouseOut(MouseOutEvent clk)
+    {
+        Debug.Log(target.name + ": Puntero fuera de elemento");
+        Debug.Log("a");
+        target.style.borderBottomColor = new Color(0, 0, 0, 1);
+        target.style.borderTopColor = new Color(0, 0, 0, 1);
+        target.style.borderLeftColor = new Color(0, 0, 0, 1);
+        target.style.borderRightColor = new Color(0, 0, 0, 1);
+        clk.StopPropagation();
     }
 }
