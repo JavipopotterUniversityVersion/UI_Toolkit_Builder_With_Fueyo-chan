5c113a8ecf3bfd05233cde47893d00e5447aa935
JaviFueyo <jfueyo@ucm.es>
2025-05-03 00:31:00 +0200

    Error: no puedo pulsar las tarjetas
    Las pulso pero me salta que no tienen info en userData

---
+++ UI_Toolkit_Builder/Assets/Scripts/Lab6/Individuo.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UIElements;
+
+namespace Lab6_namespace
+{
+    public class Individuo
+    {
+        public event Action Cambio;
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            { 
+                if(value != nombre)
+                {
+                    nombre = value;
+                    Cambio?.Invoke();
+                }
+            }
+        }
+
+        private string apellido;
+        public string Apellido
+        {
+            get { return apellido; }
+            set
+            {
+                if (value != apellido)
+                {
+                    apellido = value;
+                    Cambio?.Invoke();
+                }
+            }
+        }
+
+        //private Background image;
+
+        //public Background Image
+        //{
+        //    get { return image; }
+        //    set
+        //    {
+        //        if (value != image)
+        //        {
+        //            image = value;
+        //            Cambio?.Invoke();
+        //        }
+        //    }
+        //}
+        public Individuo(string nombre, string apellido/*, Background image*/)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            //this.image = image;
+        }
+    }
+}
---
+++ UI_Toolkit_Builder/Assets/Scripts/Lab6/Tarjeta.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Lab6_namespace
+{
+    public class Tarjeta
+    {
+        Individuo miIndividuo;
+
+        VisualElement tarjetaRoot;
+
+        Label nombreLabel;
+
+        Label apellidoLabel;
+
+        //VisualElement imagenVE;
+
+        public Tarjeta(VisualElement tarjetaRoot, Individuo individuo)
+        {
+            this.tarjetaRoot = tarjetaRoot;
+            this.miIndividuo = individuo;
+
+            nombreLabel = tarjetaRoot.Q<Label>("Nombre");
+            apellidoLabel = tarjetaRoot.Q<Label>("Apellido");
+            //imagenVE = tarjetaRoot.Q("top");
+            tarjetaRoot.userData = individuo;
+
+            tarjetaRoot
+                .Query(className: "tarjeta")
+                .Descendents<VisualElement>()
+                .ForEach(elem => elem.pickingMode = PickingMode.Ignore);
+
+            UpdateUI();
+
+            miIndividuo.Cambio += UpdateUI;
+        }
+
+        void UpdateUI()
+        {
+            nombreLabel.text = miIndividuo.Nombre;
+            apellidoLabel.text = miIndividuo.Apellido;
+            //imagenVE.style.backgroundImage = miIndividuo.Image;
+        }
+    }
+}
